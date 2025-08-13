103bfc54a06f7dc8b86d6ee128aca6383d5e7e66
Nightfall3594 <nightfall3594@gmail.com>
2025-08-13 12:16:40 +0800

    refactor(hero): migrate hero text to use BEM css and use h1

--- src/components/pages/home/hero/hero-text.jsx
+++ src/components/pages/home/hero/hero-text.jsx
@@ -2,8 +2,8 @@
 function HeroText() {
     return (
         <>
-            <p>Hi! I'm Nightfall 👋</p>
-            <p>I'm a <span>&lt;Python Developer/&gt;</span></p>
+            <h1 className="hero__text">Hi! I'm Nightfall 👋</h1>
+            <h1 className="hero__text">I'm a <span>&lt;Python Developer/&gt;</span></h1>
             <p className="hero__text--muted">I like to code because I like building stuff</p>
         </>
     )
