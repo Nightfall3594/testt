3374a793c122950b6f8680c89d8e0afb431dcf08
Nightfall3594 <nightfall3594@gmail.com>
2025-08-19 07:08:21 +0800

    feat(bottom-nav): add fade in animations for comment box

--- src/components/pages/home/bottom-nav/comment-box.jsx
+++ src/components/pages/home/bottom-nav/comment-box.jsx
@@ -1,33 +1,56 @@
 import {CloseIcon} from "../../../common/icons/index.js";
 import Overlay from "../../../common/overlays/overlay.jsx";
+import {AnimatePresence} from "framer-motion";
+
+import {motion} from "framer-motion";
+
+const commentBoxVariants = {
+    hidden: {
+        opacity: 0,
+        transition: {
+            duration: 0.2
+        }
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            duration: 0.2
+        }
+    }
+}
 
 function CommentBox({isVisible, onClose}){
-
-    if(!isVisible){
-        return null;
-    }
-
     return (
-        <>
-            <Overlay/>
-            <div className="bottom-nav__leave-message ">
-                <div className="bottom-nav-title__container">
-                    <h1 className="bottom-nav-message__title">Leave a message!</h1>
-                    <button className="bottom-nav-message__close" onClick={onClose}>
-                        <CloseIcon className="bottom-nav-close__icon"/>
-                    </button>
-                </div>
+        <AnimatePresence>
+            {isVisible &&
+                <>
+                    <Overlay/>
+                    <motion.div
+                        className="bottom-nav__leave-message "
+                        variants={commentBoxVariants}
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                    >
+                        <div className="bottom-nav-title__container">
+                            <h1 className="bottom-nav-message__title">Leave a message!</h1>
+                            <button className="bottom-nav-message__close" onClick={onClose}>
+                                <CloseIcon className="bottom-nav-close__icon"/>
+                            </button>
+                        </div>
 
-                <hr/>
+                        <hr/>
 
-                <p className="bottom-nav-message__pretext">Let me know your thoughts!</p>
-                <form action="#" method="post">
-                    <textarea name="comment" id="comment" placeholder="What do you think?"></textarea>
-                    <button type="submit" className="bottom-nav-message__send">Send</button>
-                </form>
+                        <p className="bottom-nav-message__pretext">Let me know your thoughts!</p>
+                        <form action="#" method="post">
+                            <textarea name="comment" id="comment" placeholder="What do you think?"></textarea>
+                            <button type="submit" className="bottom-nav-message__send">Send</button>
+                        </form>
 
-            </div>
-        </>
+                    </motion.div>
+                </>
+            }
+        </AnimatePresence>
     )
 }
 
